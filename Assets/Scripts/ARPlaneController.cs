e1511f103350f37e591317bf8640490b728ea7b6
Bhagya Wijenayake <bhagyasudaraka98@gmail.com>
2023-08-29 18:12:39 +0530

    disable surface detection after placed fixed

--- Assets/Scripts/ARPlaneController.cs
+++ Assets/Scripts/ARPlaneController.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
 [RequireComponent(typeof(ARPlaneManager))]
-
 public class ARPlaneController : MonoBehaviour
 {
     ARPlaneManager m_ARPlaneManager;
+
     void Awake()
     {
         m_ARPlaneManager = GetComponent<ARPlaneManager>();
@@ -15,41 +15,49 @@ public class ARPlaneController : MonoBehaviour
 
     void OnEnable()
     {
-        m_ARPlaneManager.onPlacedObject += DisablePlaneDetection;
+        PlaceObjectOnPlane.OnPlacedObject += DisablePlaneDetection;
     }
 
     void OnDisable()
     {
-        m_ARPlaneManager.onPlacedObject -= DisablePlaneDetection;
+        PlaceObjectOnPlane.OnPlacedObject -= DisablePlaneDetection;
     }
 
+    /// <summary>
+    /// Disables plane detection and hides existing planes.
+    /// </summary>
     void DisablePlaneDetection()
     {
-       //planeDetectionMessage = "Disable Plane Detection and Hide Existing Planes";
-       SetAllPlanesActive(false);
-         m_ARPlaneManager.enabled = false;
+        SetAllPlanesActive(false);
+        m_ARPlaneManager.enabled = !m_ARPlaneManager.enabled;
     }
 
-    //summary
-    //Iterates over all the existing planes and activates
-    //or deactivates their <c>GameObject</c>s.
-    //summary
-
+    /// <summary>
+    /// Iterates over all the existing planes and activates
+    /// or deactivates their <c>GameObject</c>s.
+    /// </summary>
     void SetAllPlanesActive(bool value)
     {
         foreach (var plane in m_ARPlaneManager.trackables)
+        {
             plane.gameObject.SetActive(value);
+        }
+    }
+
+    void OnPlanesChanged(ARPlanesChangedEventArgs args)
+    {
+        // You can handle plane changes here if needed
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Initialization code if needed
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Update logic if needed
     }
 }
